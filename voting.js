75615d1667c8657d57110c3f61fa4e13554ba59b
Onifade Dolapo <hardolipey@gmail.com>
2020-08-21 14:26:42 +0100

    Working on the Leaderboard segment

--- voting.js
+++ voting.js
@@ -53,3 +53,68 @@ function show(){
 // function hide(){ 
 //     document.getElementById("part").style.visibility = "hidden";
 }
+let usersList = "";
+let user = [
+    {
+        name : 'Dorathy',
+        image : "images/Ellipse 1.png",
+        votes : 0,
+       
+    },
+    {
+        name : 'Laycon',
+        image : "images/laycon.jpg",
+        votes : 0,
+       
+    },
+    {
+        name : 'Nengi',
+        image : "images/Nengi.jpg",
+        votes : 0,
+       
+    },
+    {
+        name : 'Prince',
+        image : "images/prinebbn.jpg",
+        votes : 0,
+       
+    },
+    {
+        name : 'Ozo',
+        image : 'images/ozo.jpg',
+        votes : 0,
+       
+    },
+    {
+        name : 'Erica',
+        image : "images/erica.jpg",
+        votes : 0,
+       
+    }
+]
+user.forEach (user => {
+    usersList = `<div class="row votingbox >
+                    <div class="col-lg-5">
+                    <div class="media">
+                        <img src="${user.image}" alt="${user.name}" class="mt-3 mr-3 rounded-circle" style="width:60px;">
+                        <div class="media-body">
+                        <h4 class="pt-2">${user.name}</h4>
+                        <h2 class ="pt-2">${user.votes}</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-5">
+                    <div class="media">
+                        <img src="${user.image}" alt="${user.name}" class="mt-3 mr-3 rounded-circle" style="width:60px;">
+                        <div class="media-body">
+                        <h4 class="pt-2">${user.name}</h4>
+                        <h2 class ="pt-2">${user.votes}</h2>
+                        </div>
+                    </div>
+                </div>
+                
+                </div>`
+                console.log (usersList);
+                document.getElementById("segment2").innerHTML += usersList;
+})  
+console.log(user);
